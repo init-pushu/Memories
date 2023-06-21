07b8bff632aae3e500ca5066203dd95caba8da8a
init-pushu <115545185+init-pushu@users.noreply.github.com>
2023-06-21 18:54:06 +0530

    build uploaded

---
+++ client/build/precache-manifest.506e66816d94edd89102b03c7a79e4ba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260413cb51e0f2db74ef35f817c662b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ce4698fb7774af4093a",
+    "url": "/static/css/main.10cfd0a1.chunk.css"
+  },
+  {
+    "revision": "5b18ed2ffedd83d20f04",
+    "url": "/static/js/2.6ca31f13.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.6ca31f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce4698fb7774af4093a",
+    "url": "/static/js/main.ab1158ed.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "3a357df7b06e01105724056f894d755f",
+    "url": "/static/media/bgg.3a357df7.jpg"
+  },
+  {
+    "revision": "ec0f8fb6bfaeda58a47064176ba34985",
+    "url": "/static/media/download.ec0f8fb6.jpg"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
